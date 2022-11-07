283e6ad6227bbd29921b993cc3a0ce1933b6b289
KalyaevPhilipp <buldog9595@mail.ru>
2022-11-07 15:31:13 +0300

    Fixed MainActivity.kt

--- app/src/main/java/ru/philipp_kalyaev/android/education_api_git/MainActivity.kt
+++ app/src/main/java/ru/philipp_kalyaev/android/education_api_git/MainActivity.kt
@@ -2,44 +2,30 @@ package ru.philipp_kalyaev.android.education_api_git
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import ru.philipp_kalyaev.android.education_api_git.UI.DetailsFragment
 import ru.philipp_kalyaev.android.education_api_git.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var getDetail: Button
-    private lateinit var binding_main: ActivityMainBinding
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding_main = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding_main.root)
-        val fragment = supportFragmentManager.findFragmentById(R.id.container)
-        if (fragment != null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, fragment)
-                .addToBackStack(null)
-                .commit()
-        }
-        getDetail = binding_main.getFragment
-        val detailFrag: DetailsFragment = DetailsFragment()
-        getDetail.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, detailFrag)
-                .addToBackStack(null)
-                .commit()
-        }
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val detailFrag = DetailsFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, detailFrag)
+            .addToBackStack(null)
+            .commit()
     }
 
-    /*
-    getDetail.setOnClickListener {
-        setContentView(binding_frag.root)
-    }*/
-
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
--- app/src/main/java/ru/philipp_kalyaev/android/education_api_git/UI/DetailsFragment.kt
+++ app/src/main/java/ru/philipp_kalyaev/android/education_api_git/UI/DetailsFragment.kt
@@ -7,17 +7,19 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.philipp_kalyaev.android.education_api_git.databinding.DetailFragmentBinding
 
-class DetailsFragment: Fragment(){
-    private var binding : DetailFragmentBinding?=null
+class DetailsFragment : Fragment() {
+    private var binding: DetailFragmentBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = DetailFragmentBinding.inflate(inflater,container,false)
-        return view.root
+        binding = DetailFragmentBinding.inflate(inflater, container, false)
+        return binding!!.root
 
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
